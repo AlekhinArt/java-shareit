dc8da961d66515b92b9fa8877bdfedfeb06f3598
Artem024 <alekhinart@yandex.ru>
2022-12-10 02:25:18 +0300

    ref

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingState.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingState.java
@@ -10,13 +10,5 @@ public enum BookingState {
     PAST,
     REJECTED,
     WAITING;
-
-    public static Optional<BookingState> from(String stringState) {
-        for (BookingState state : values()) {
-            if (state.name().equalsIgnoreCase(stringState)) {
-                return Optional.of(state);
-            }
-        }
-        return Optional.empty();
-    }
+    
 }
